d66ccc1540591972ec88c355a05e36e00fdb41ef
yjy <yjy@ekuaibao.com>
2020-06-21 20:19:19 +0800

    update

--- src/components/ef/mixins.js
+++ src/components/ef/mixins.js
@@ -50,7 +50,7 @@ export const easyFlowMixin = {
                     // 线的颜色
                     stroke: '#E0E3E7',
                     // 线的粗细，值越大线越粗
-                    strokeWidth: 2,
+                    strokeWidth: 3,
                     // 设置外边线的颜色，默认设置透明，这样别人就看不见了，点击线的时候可以不用精确点击，参考 https://blog.csdn.net/roymno2/article/details/72717101
                     outlineStroke: 'transparent',
                     // 线外边的宽，值越大，线的点击范围越大
